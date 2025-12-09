1015219eee2f0c81f683585ab36c5455a302be2e
Admin <Dreanj44@gmail.com>
2025-12-09 21:17:59 +0100

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomeLayout } from './pages';
+
+const router = createBrowserRouter([{ path: "/", element: <HomeLayout/> }])
+
+createRoot(document.getElementById('root')!).render(<RouterProvider router={router}/>);
---
+++ src/utils/links.tsx
@@ -0,0 +1,14 @@
+export type Link = {
+    ref: string;
+    label: string;
+
+};
+
+export const links: Link[] = [
+    {ref: "/", label:"home"} ,
+    {ref: "/spacex", label:"spacex"} ,
+    {ref: "/webb", label:"webb"} ,
+    {ref: "/hubble", label:"hubble"} ,
+    {ref: "/news", label:"news"} ,
+    {ref: "/apod", label:"NASA's day photo"}
+]
