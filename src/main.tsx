39333f868cdce7f7fc374e0ed7c0bd048bb11523
Admin <Dreanj44@gmail.com>
2025-12-09 22:04:18 +0100

    add routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,24 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { HomeLayout } from './pages';
+import { Apod, HomeLayout, Hubble, Landing, News, SpaceX, Webb } from "./pages";
 
-const router = createBrowserRouter([{ path: "/", element: <HomeLayout/> }])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    children: [
+        { index: true, element: <Landing /> },
+        { path: "news", element: <News />},
+        { path: "webb", element: <Webb />},
+        { path: "spacex", element: <SpaceX />},
+        { path: "apod", element: <Apod />},
+        { path: "hubble", element: <Hubble />},
 
-createRoot(document.getElementById('root')!).render(<RouterProvider router={router}/>);
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
